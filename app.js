c3568004c9a620559e00d685618d940dfb1ecf40
nafiul-shishir <nafiul.shishir@gmail.com>
2021-06-28 04:53:33 +0600

    walton project file

---
+++ app.js
@@ -0,0 +1,48 @@
+const intro = document.querySelector('.intro');
+const video = intro.querySelector('Img');
+const text = intro.querySelector('h1'); 
+
+//End Section
+
+const section = document.querySelector('section');
+const end = section.querySelector('h1');
+
+//Scrollmagic
+
+const controller = new ScrollMagic.controller();
+const scene = new ScrollMagic.Scene({
+    duration:1000,
+    triggerElement: intro,
+    triggerHook:0
+
+})
+.addIndicators()
+.setPin(intro)
+.addTo(controller);
+
+//Text Animation
+const textAnim = TweenMax.fromTo(text, 3, {opacity:1},{opacity:0});
+
+let scene2 = new ScrollMagic.Scene({
+    duration: 3000,
+    triggerElement: intro,
+    triggerHook:0
+});
+
+//animation
+
+let accelamount=0.1;
+let scrollpos =0;
+let delay = 0;
+
+scene.on("update", e=>{
+    scrollpos =e.scrollPos/ 1000;
+    
+});
+
+setInterval(() => {
+    delay+=(scrollpos -delay)* accelamount;
+    Img.currentTime = scrollpos;
+}, 33.3)
+
+
